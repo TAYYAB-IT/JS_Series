312a5cbc7425b3f5cdf7840695d2fdc59ab2bdea
Tayyab Aman <tayyabaman80@gmail.com>
2021-12-13 13:08:40 +0500

    Annother Method

--- Promises-Asyn-Await.js
+++ Promises-Asyn-Await.js
@@ -10,5 +10,24 @@ async function check(x){
     });
     console.log(await promis)
 }
-arr=[18,29,14,15,,9,0,12,34,25,29]
-arr.forEach(check);
+var arr=[18,29,14,15,9,0,12,34,25,29]
+//arr.forEach(check);
+
+// Another format
+function check2(x){
+    let promis=new Promise((resolve,reject)=>{
+        if(x>=18){
+           resolve(" is an Adult Age")
+         }
+    
+         reject("Below then 18")
+        })
+        promis.then((value)=>{
+            console.log(value)
+        },
+        (err)=>{
+            console.log(err)})
+}
+
+
+arr.forEach(check2);
