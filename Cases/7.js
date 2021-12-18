d345a025e9a73a1fb46f43d54341debaa2492724
Tayyab Aman <tayyabaman80@gmail.com>
2021-12-18 13:00:21 +0500

    2nd

--- Cases/7.js
+++ Cases/7.js
@@ -1,10 +1,10 @@
 function birthdayCakeCandles(candles) {
     // Write your code here
 max=Math.max.apply(0,candles)
-console.log((candles.filter((val)=>
+return((candles.filter((val)=>
     val==max
 )).length)
 }
 n=4
 candles=[3 ,2 ,1, 3]
-birthdayCakeCandles(candles)
+consolr.log(birthdayCakeCandles(candles))
