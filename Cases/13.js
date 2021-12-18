e00a7d51456afb84c377fdccbebd11b6755c6789
Tayyab Aman <tayyabaman80@gmail.com>
2021-12-18 15:11:34 +0500

    2nd

--- Cases/13.js
+++ Cases/13.js
@@ -1,9 +1,14 @@
 function birthday(s, d, m) {
+  var sum = 0;
     // Write your code here
     count=s.filter((value,index)=>{
-        sum=0
-        x=index
-        for(i=0;i<m&&x<=m;i++){
+      if(m==1){
+        var  sum=value
+        var x=index+1}
+      else{
+        var  sum=0
+        var x=index} 
+        for(i=0;i<m&&x<=(s.length-1);i++){
            sum+=s[x]
            x+=1
         }
@@ -11,8 +16,8 @@ function birthday(s, d, m) {
     })
 return count.length
 }
-const n=5
-const arr=[1, 2, 1, 3, 2]
-const d=3
-const m=2
+const n=2
+const arr=[1,4]
+const d=4
+const m=1
 console.log(birthday(arr,d,m))
