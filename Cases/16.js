a1e975b837f289dfdc9b8244705b38060ba9bbcb
Tayyab Aman <tayyabaman80@gmail.com>
2021-12-19 01:21:41 +0500

    2nd

--- Cases/16.js
+++ Cases/16.js
@@ -1,21 +1,25 @@
 function dayOfProgrammer(year) {
     // Write your code here
     //Programmer day is 256th 
-leap=false
+var leap=false
 var d= new Date()
-if(year%4==0 ){
+if(year>=1700 && year<=1917){
+    if(year%4==0){leap=true}
+}
+else if((year%4==0 &&year%100!=0)||year%400==0){
     leap=true
 }
-days_before= 243
+var days_before= 243
 if (leap){
-d.setFullYear(year,08,256-(days_before+1))
+d.setFullYear(year,8,256-(days_before+1))
 //console.log("leap",256-(days_before+1))
 }
 else{
-    d.setFullYear(year,08,256-days_before)
+    d.setFullYear(year,8,256-days_before)
 }
-date=d.getDate()
-month=d.getMonth()+1
+var date=d.getDate()
+if(year==1918){ date+=13} // The transition from the Julian to Gregorian calendar system occurred in 1918, when the next day after January 31st was February 14th.
+var month=d.getMonth()+1
 if(date<10){
     date="0"+month
 }
@@ -24,5 +28,5 @@ if(month<10){
 }
 return (date+"."+month+"."+d.getFullYear())
 }
-const year=2017
+const year=1918
 console.log(dayOfProgrammer(year))
