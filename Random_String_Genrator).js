fba705ed74d31e48a2d403a68fecc070725d38b9
Tayyab Aman <95743660+TAYYAB-IT@users.noreply.github.com>
2022-02-23 15:26:09 +0500

    Update Random_String_Genrator).js

--- Random_String_Genrator).js
+++ Random_String_Genrator).js
@@ -1,18 +1,12 @@
-// dec2hex :: Integer -> String
-// i.e. 0-255 -> '00'-'ff'
-function dec2hex (dec) {
-  return dec.toString(16).padStart(2, "0")
-}
-
-// generateId :: Integer -> String
-function generateId (len=40) {
-  var arr = new Uint8Array((len ) / 2)
-  window.crypto.getRandomValues(arr)
-  return Array.from(arr, dec2hex).join('')
-}
-
-console.log(generateId()) //By Default length 40
-// "82defcf324571e70b0521d79cce2bf3fffccd69"
-
-console.log(generateId(20)) //pass length to function
-// "c1a050a4cd1556948d41"
+const Random_genrator=async(length=7)=>{
+      function randomString(len) {
+        charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var randomString = '';
+        for (var i = 0; i < len; i++) {
+            var randomPoz = Math.floor(Math.random() * charSet.length);
+            randomString += charSet.substring(randomPoz,randomPoz+1);
+        }
+        return randomString;
+    }
+    return  randomString(length);
+ }
